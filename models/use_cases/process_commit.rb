614fcbdc459fb84faa5bd0a6c78a2d056b4f6623
Vanja Radovanović <elvanja@gmail.com>
2013-08-26 22:19:01 +0200

    added specs for process commit use case

--- models/use_cases/process_commit.rb
+++ models/use_cases/process_commit.rb
@@ -4,6 +4,11 @@ require_relative '../services/get_jenkins_projects'
 
 module GitlabWebHook
   class ProcessCommit
+    def initialize(get_jenkins_projects = GetJenkinsProjects.new, create_project_for_branch = CreateProjectForBranch.new)
+      @get_jenkins_projects = get_jenkins_projects
+      @create_project_for_branch = create_project_for_branch
+    end
+
     def with(details, action)
       projects = get_projects_to_process(details)
 
@@ -18,10 +23,10 @@ module GitlabWebHook
 
     def get_projects_to_process(details)
       if Settings.automatic_project_creation?
-        projects = GetJenkinsProjects.new.exactly_matching(details)
-        projects << CreateProjectForBranch.new.with(details) if projects.empty?
+        projects = @get_jenkins_projects.exactly_matching(details)
+        projects << @create_project_for_branch.with(details) if projects.empty?
       else
-        projects = GetJenkinsProjects.new.matching(details)
+        projects = @get_jenkins_projects.matching(details)
       end
 
       raise NotFoundException.new("no project references the given repo url and commit branch") if projects.empty?

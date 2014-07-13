d6371c26cc2429040ba4252a94cf9b353337ff38
Vanja Radovanović <elvanja@gmail.com>
2014-07-13 12:40:49 +0200

    issue #30 related added crumb exclusion for the hook

--- models/root_action.rb
+++ models/root_action.rb
@@ -6,9 +6,11 @@ require_relative 'api'
 class GitlabWebHookRootAction < Jenkins::Model::UnprotectedRootAction
   include Jenkins::RackSupport
 
+  WEB_HOOK_ROOT_URL = "gitlab"
+
   display_name "Gitlab Web Hook"
   icon nil # we don't need the link in the main navigation
-  url_path "gitlab"
+  url_path WEB_HOOK_ROOT_URL
 
   def call(env)
     GitlabWebHook::Api.new.call(env)

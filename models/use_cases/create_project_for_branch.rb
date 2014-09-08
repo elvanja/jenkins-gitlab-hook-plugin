de2843b81ff55696c52a9f9d8178e5a5a51163aa
Javier Palacios <javier.palacios@fon.com>
2014-09-08 12:29:52 +0000

    Add method to retrieve the template project

--- models/use_cases/create_project_for_branch.rb
+++ models/use_cases/create_project_for_branch.rb
@@ -39,6 +39,11 @@ module GitlabWebHook
       @get_jenkins_projects.master(details) || raise(NotFoundException.new(master_not_found_message))
     end
 
+    def get_template_project(template ,details)
+      template_not_found_message = "could not found given template, please create a disabled project named #{template}"
+      @get_jenkins_projects.named(template) || raise(NotFoundException.new(template_not_found_message))
+    end
+
     def get_new_project_name(copy_from, details)
       new_project_name = "#{Settings.use_master_project_name? ? copy_from.name : details.repository_name}_#{details.safe_branch}"
       raise ConfigurationException.new("project #{new_project_name} already exists") unless @get_jenkins_projects.named(new_project_name).empty?

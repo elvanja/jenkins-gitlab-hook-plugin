elvanja/jenkins-gitlab-hook-plugin

commit 698b97afe52091a4376bb0cd8936a87464546bd5

BUGFIX : search templates only for unknown repositories
When the git repo is known but not triggered, it should be avoided to
go through the template based job creation.

models/use_cases/process_commit.rb
@@ -29,7 +29,7 @@ module GitlabWebHook
         projects = @get_jenkins_projects.matching(details)
       end
 
-      if projects.empty?
+      if @get_jenkins_projects.matching_uri(details).empty?
         Settings.templated_jobs.each do |matchstr,template|
           if details.repository_name.start_with? matchstr
             projects << @create_project_for_branch.from_template(template, details)

models/services/get_jenkins_projects.rb
@@ -18,6 +18,13 @@ java_import Java.hudson.plugins.git.util.DefaultBuildChooser
 
 module GitlabWebHook
   class GetJenkinsProjects
+
+    def matching_uri(details)
+      all.select do |project|
+        project.matches_uri?(details.repository_uri)
+      end
+    end
+
     def matching(details, exactly = false)
       all.select do |project|
         project.matches?(details.repository_uri, details.branch, exactly)

models/values/project.rb
@@ -32,10 +32,14 @@ module GitlabWebHook
       @logger = logger
     end
 
-    def matches?(details_uri, branch, exactly = false)
+    def matches_uri?(details_uri)
       return false unless buildable?
       return false unless git?
-      return false unless matches_repo_uri?(details_uri)
+      matches_repo_uri?(details_uri)
+    end
+
+    def matches?(details_uri, branch, exactly = false)
+      return false unless matches_uri?(details_uri)
       matches_branch?(branch, exactly).tap { |matches| logger.info("project #{self} #{matches ? "matches": "doesn't match"} the #{branch} branch") }
     end
 

commit 4e29000268c6b5de238f945085439e548767b8df

BUGFIX : search templates only if no match was found
This behaviour was broken by commit 698b97afe52091a4376bb0cd8936a87464546bd5

models/use_cases/process_commit.rb
@@ -29,7 +29,7 @@ module GitlabWebHook
         projects = @get_jenkins_projects.matching(details)
       end
 
-      if @get_jenkins_projects.matching_uri(details).empty?
+      if projects.empty? && @get_jenkins_projects.matching_uri(details).empty?
         settings.templated_jobs.each do |matchstr,template|
           if details.repository_name.start_with? matchstr
             projects << @create_project_for_branch.from_template(template, details)
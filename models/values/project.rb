2f504c97f3fd206e52e460e1599fb459092db2a7
Javier Palacios <javiplx@gmail.com>
2014-10-22 19:49:13 +0200

    Make matches_uri return true for disabled projects

--- models/values/project.rb
+++ models/values/project.rb
@@ -33,12 +33,12 @@ module GitlabWebHook
     end
 
     def matches_uri?(details_uri)
-      return false unless buildable?
       return false unless git?
       matches_repo_uri?(details_uri)
     end
 
     def matches?(details_uri, branch, exactly = false)
+      return false unless buildable?
       return false unless matches_uri?(details_uri)
       matches_branch?(branch, exactly).tap { |matches| logger.info("project #{self} #{matches ? "matches": "doesn't match"} the #{branch} branch") }
     end

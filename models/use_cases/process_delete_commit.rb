64c2fdb1759e52adadb3c1a30255f38befcf34f6
Javier Palacios <javiplx@gmail.com>
2014-10-25 15:43:54 +0200

    BUGFIX : calling private method

--- models/use_cases/process_delete_commit.rb
+++ models/use_cases/process_delete_commit.rb
@@ -14,7 +14,7 @@ module GitlabWebHook
       return ["branch #{commit_branch} is deleted, but relates to master project so will not delete, skipping processing"] if commit_branch == settings.master_branch
 
       messages = []
-      @get_jenkins_projects.all.select do |project|
+      @get_jenkins_projects.matching_uri(details).select do |project|
         project.matches?(details.repository_uri, details.branch, true)
       end.each do |project|
         messages << "project #{project} matches deleted branch but is not automatically created by the plugin, skipping" and next unless project.description.match /#{settings.description}/

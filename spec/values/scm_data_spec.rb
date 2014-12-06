1002b706815ac18f9693f07f9c999aad54f8b439
Javier Palacios <javiplx@gmail.com>
2014-12-06 18:04:08 +0100

    Fix tests broken by UserRemoteConfig changes

--- spec/values/scm_data_spec.rb
+++ spec/values/scm_data_spec.rb
@@ -5,7 +5,6 @@ module GitlabWebHook
     let(:details) { double(RequestDetails, repository_name: 'discourse', branch: 'features_meta', safe_branch: 'features_meta') }
     let(:remote_config) { double(UserRemoteConfig, getUrl: 'http://localhost/diaspora', getName: 'Diaspora', getCredentialsId: 'id', getRefspec: ['+refs/heads/*:refs/remotes/origin/*']) }
     let(:source_scm) { double(GitSCM, getScmName: 'git', getUserRemoteConfigs: [remote_config, double(UserRemoteConfig)]).as_null_object }
-    let(:branchspec) { double(BranchSpec) }
     let(:subject) { described_class.new(source_scm, details) }
 
     context 'when building' do
@@ -30,22 +29,26 @@ module GitlabWebHook
       end
 
       context 'when determining branch' do
+        let(:branchspec) { double(BranchSpec) }
+        let(:java_branchspec) { double(BranchSpec) }
         context 'without source scm name' do
           before(:each) do
-            expect(BranchSpec).to receive(:new).with('features_meta').and_return(:branchSpec)
+            expect(branchspec).to receive(:java_object).and_return(java_branchspec)
+            expect(BranchSpec).to receive(:new).with('features_meta').and_return(branchspec)
           end
           it 'uses details branch only' do
             expect(remote_config).to receive(:getName).and_return(nil)
-            expect(subject.branchlist).to eq([:branchSpec])
+            expect(subject.branchlist).to eq([java_branchspec])
           end
         end
 
         context 'with source scm name present' do
           before(:each) do
-          expect(BranchSpec).to receive(:new).with('Diaspora/features_meta').and_return(:branchspec)
+            expect(branchspec).to receive(:java_object).and_return(java_branchspec)
+            expect(BranchSpec).to receive(:new).with('Diaspora/features_meta').and_return(branchspec)
           end
           it 'prefixes details branch' do
-            expect(subject.branchlist).to eq([:branchspec])
+            expect(subject.branchlist).to eq([java_branchspec])
           end
         end
       end

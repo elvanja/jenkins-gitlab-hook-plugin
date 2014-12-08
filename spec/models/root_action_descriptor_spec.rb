4a732918d9359aa9a2ed645499e956458483a977
Javier Palacios <javiplx@gmail.com>
2014-12-08 11:24:07 +0100

    Test reading parameters from disk file

--- spec/models/root_action_descriptor_spec.rb
+++ spec/models/root_action_descriptor_spec.rb
@@ -81,4 +81,48 @@ describe GitlabWebHookRootActionDescriptor do
       end
     end
 
+    context 'read disk descriptor' do
+
+      let(:xml_file) { double(exists: true, canonicalPath: 'spec/fixtures/descriptor.xml' ) }
+      let(:config_file) { double('configFile', file: xml_file) }
+      let(:subject) { GitlabWebHookRootActionDescriptor.new }
+
+      before(:each) do
+        expect(subject).to receive(:configFile) { config_file }
+      end
+
+        it '#automatic_project_creation?' do
+          expect(subject.automatic_project_creation?).to be true
+        end
+
+        it '#master_branch' do
+          expect(subject.master_branch).to eq 'primary'
+        end
+
+        it '#use_master_project_name?' do
+          expect(subject.use_master_project_name?).to be true
+        end
+
+        it '#description' do
+          expect(subject.description).to eq 'Alternate description'
+        end
+
+        it '#any_branch_pattern' do
+          expect(subject.any_branch_pattern).to eq 'origin/*'
+        end
+
+        it '#template_fallback' do
+          expect(subject.template_fallback).to eq 'default_project'
+        end
+
+        it '#templated_groups' do
+          expect(subject.templated_groups).to eq( { 'android' => 'gradle_project' } )
+        end
+
+        it '#templated_jobs' do
+          expect(subject.templated_jobs).to eq( { 'webapp-' => 'maven_project' , 'java-lib-' => 'artifactory_project' } )
+        end
+
+    end
+
 end
